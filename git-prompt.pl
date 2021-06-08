ca8a0f56f9bd2198f39b3915889c7614e8f30d2a
Buraddo23 <vladsing@gmail.com>
2021-06-08 17:44:43 +0300

    Added changetype counting

--- git-prompt.pl
+++ git-prompt.pl
@@ -19,10 +19,29 @@ my $prompt="${green}[%n${blue}@%M] ${white}%B%c3%b";
 my $git_branch=`/usr/bin/env git rev-parse --abbrev-ref HEAD 2> /dev/null`;
 chomp $git_branch;
 
-my $git_status=0;
+my $git_status = 0;
+my %index = ('M', 0,
+             'A', 0,
+             'D', 0);
+my %work_dir = ('M', 0,
+                'A', 0,
+                'D', 0);
+my %extra = ('?', 0,
+             '!', 0);
+
 if ($git_branch ne "") {
 	open IF, "/usr/bin/env git status --porcelain 2> /dev/null|" or exit;
 	while (<IF>) {
+		$index{'M'}++ if ($_ =~ /^.M/);
+		$index{'A'}++ if ($_ =~ /^.A/);
+		$index{'D'}++ if ($_ =~ /^.D/);
+		$index{'M'}++ if ($_ =~ /^.R/);
+		$work_dir{'M'}++ if ($_ =~ /^M/);
+		$work_dir{'A'}++ if ($_ =~ /^A/);
+		$work_dir{'D'}++ if ($_ =~ /^D/);
+		$work_dir{'M'}++ if ($_ =~ /^R/);
+		$extra{'?'}++ if ($_ =~ /^??/);
+		$extra{'!'}++ if ($_ =~ /^!!/);
 		$git_status++;
 	}
 	
@@ -31,7 +50,10 @@ if ($git_branch ne "") {
 	} else {
 		$prompt .= "${green} ";
 	}
-	$prompt .= "{${git_branch}}"
+	$prompt .= "{${git_branch} +$index{'A'} ~$index{'M'} -$index{'D'}";
+	$prompt .= " ?$extra{'?'}" if ($extra{'?'});
+	$prompt .= " !$extra{'!'}" if ($extra{'!'});
+	$prompt .= " | +$work_dir{'A'} ~$work_dir{'M'} -$work_dir{'D'}}";
 }
 $prompt .= "${end}> ";
 	
